133f3e8d54ede1763620f0e99021d395ec964c7b
Mekhala Joshi <mekhalajoshi@gmail.com>
2018-06-10 13:38:58 +0530

    Cracking The Coding Interview

---
+++ programming/src/string/IsUniqueChars.java
@@ -0,0 +1,36 @@
+package string;
+
+/***
+ * Implement an algorithm to determine if a string has all unique characters 
+ * What if you can not use additional data structures? --n^2 Solution
+ */
+import java.util.HashSet;
+
+public class IsUniqueChars {
+
+	public boolean isUniqueChars(String s) {
+		if (s == null) {
+			return false;
+		}
+		HashSet<Character> set = new HashSet<Character>();
+		// boolean isUnique = true;
+		for (char c : s.toCharArray()) {
+			if (set.contains(c)) {
+				return false;
+			} else {
+				set.add(c);
+			}
+
+		}
+		return true;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("ABC " + new IsUniqueChars().isUniqueChars("ABC"));
+		System.out.println("aaa " + new IsUniqueChars().isUniqueChars("aaa"));
+		System.out.println("b..a " + new IsUniqueChars().isUniqueChars("b..a"));
+		System.out.println("b.a " + new IsUniqueChars().isUniqueChars("b.a"));
+	}
+
+}
