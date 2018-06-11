a84c5c20de7a0f610b020b555fac3ab5bf171622
Mekhala Joshi <mekhalajoshi@gmail.com>
2018-06-11 15:44:31 +0530

    tree

--- programming/src/trees/BinaryTree.java
+++ programming/src/trees/BinaryTree.java
@@ -311,6 +311,41 @@ public class BinaryTree {
 		return isBalancedTree(A) ? 1 : 0;
 	}
 
+	/***
+	 * Given values of two values n1 and n2 in a Binary Search Tree, find the Lowest
+	 * Common Ancestor (LCA). You may assume that both the values exist in the tree.
+	 * 
+	 * BST_LCA LCA of 10 and 14 is 12 LCA of 14 and 8 is 8 LCA of 10 and 22 is 20
+	 * Following is definition of LCA from Wikipedia: Let T be a rooted tree. The
+	 * lowest common ancestor between two nodes n1 and n2 is defined as the lowest
+	 * node in T that has both n1 and n2 as descendants (where we allow a node to be
+	 * a descendant of itself).
+	 * 
+	 * The LCA of n1 and n2 in T is the shared ancestor of n1 and n2 that is located
+	 * farthest from the root. Computation of lowest common ancestors may be useful,
+	 * for instance, as part of a procedure for determining the distance between
+	 * pairs of nodes in a tree: the distance from n1 to n2 can be computed as the
+	 * distance from the root to n1, plus the distance from the root to n2, minus
+	 * twice the distance from the root to their lowest common ancestor. (Source
+	 * Wiki)
+	 */
+	public Node lca(Node root,int v1,int v2){
+	    Node current = root;
+	    if(root == null){
+	      return null;
+	    }
+	    while(current!=null){
+	        if(v1<current.data && v2<current.data){
+	            current = current.left;
+	        }else if(v1>current.data && v2>current.data){
+	            current = current.right;
+	        }else{
+	            return current;
+	        }
+	    }
+	    return current;
+	}
+
 	public static void main(String[] args) {
 		BinaryTree tree = new BinaryTree();
 		BinaryTree t = new BinaryTree();
